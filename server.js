647bc7138ee3580d78e333ce68586a67ecb529b0
Ariel <arieltanami122@gmail.com>
2025-07-09 14:53:01 +0300

    Add "BEFORE code" in server.js

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ app.use(express.json());
 app.use(greetRouter);
 app.use("/users", usersRouter);
 
+//#region "Before" code
+const posts = [{ id: 1, title: "Hello" }];
+const comments = { 1: [] };
+
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+
+app.get("/posts/:postId/comments", (req, res) => {
+  const pid = req.params.postId;
+  res.json(comments[pid] || []);
+});
+
+app.post("/posts/:postId/comments", (req, res) => {
+  const pid = req.params.postId;
+  const newComment = { id: Date.now(), text: req.body.text };
+  comments[pid] = comments[pid] || [];
+  comments[pid].push(newComment);
+  res.status(201).json(newComment);
+});
+//#endregion "Before" code
+
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
