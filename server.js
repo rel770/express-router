4fcbfc3a6d799280f00215667077540fe52f45f0
Ariel <arieltanami122@gmail.com>
2025-07-09 16:12:23 +0300

    Use posts & commented routes in server.js

--- server.js
+++ server.js
@@ -1,38 +1,18 @@
 import express from "express";
 import greetRouter from "./routes/greet.js";
 import usersRouter from "./routes/users.js";
+import postsRouter from "./routes/posts.js";
 
 const PORT = 3000;
 
 const app = express();
+
+// body parser middleware
 app.use(express.json());
 
 // routes
 app.use(greetRouter);
 app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
 
-//#region "Before" code
-const posts = [{ id: 1, title: "Hello" }];
-const comments = { 1: [] };
-
-app.get("/posts", (req, res) => {
-  res.json(posts);
-});
-
-app.get("/posts/:postId/comments", (req, res) => {
-  const pid = req.params.postId;
-  res.json(comments[pid] || []);
-});
-
-app.post("/posts/:postId/comments", (req, res) => {
-  const pid = req.params.postId;
-  const newComment = { id: Date.now(), text: req.body.text };
-  comments[pid] = comments[pid] || [];
-  comments[pid].push(newComment);
-  res.status(201).json(newComment);
-});
-//#endregion "Before" code
-
-app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
