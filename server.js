66db904ea96643bdb73d81dcd50ff41e38d5e036
Ariel <arieltanami122@gmail.com>
2025-07-09 12:17:08 +0300

    Use usersRouter in server.js file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-import express from 'express';
-import greetRouter from './routes/greet.js';
+import express from "express";
+import greetRouter from "./routes/greet.js";
+import usersRouter from "./routes/users.js";
 
 const PORT = 3000;
 
@@ -8,23 +9,7 @@ app.use(express.json());
 
 // routes
 app.use(greetRouter);
-
-//#region before example
-let users = [{ id: 1, name: 'Alice' }];
-
-app.get('/users', (req, res) => {
-  res.json(users);
-});
-
-app.post('/users', (req, res) => {
-  const newUser = { id: Date.now(), name: req.body.name };
-  users.push(newUser);
-  res.status(201).json(newUser);
-});
-
-// More routes...
-
-//#endregion before example
+app.use("/users", usersRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
